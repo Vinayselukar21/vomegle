c804b73d2bd0f93c3778497395454f4061e6a8ee
Vinay <vselukar2102@gmail.com>
2024-12-25 22:48:40 +0530

    direct url in vite config

--- frontend/omegle-clone/vite.config.ts
+++ frontend/omegle-clone/vite.config.ts
@@ -1,7 +1,6 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-
 export default defineConfig({
   plugins: [react()],
   resolve: {
@@ -12,7 +11,7 @@ export default defineConfig({
    server: {
         proxy: {
             '/socket.io': {
-                target: 'http://localhost:3000',
+                target: "https://vomegle.onrender.com",
                 ws: true,
             },
         },
